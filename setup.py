2d487688c2bb8d000b6a5e6bdf87d57db470e70f
Maik Luu Bach <maik.luu-bach@tum.de>
2023-04-30 19:57:38 +0200

    torch 1.4.0 added

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    package_dir={"": "gfl"},
     package_data={"": ["resources/*"]},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
